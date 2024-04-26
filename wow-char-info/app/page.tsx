c301fca462cf94277b6745ed116365998d53fd83
nikola.ninov <momzzze@abv.bg>
2024-04-26 15:18:23 +0300

    implement toggleMode button for light dark theme
    add button from shadcn
    add dropdown menu from shadcn

--- wow-char-info/app/page.tsx
+++ wow-char-info/app/page.tsx
@@ -1,18 +1,22 @@
+import { ModeToggle } from "@/components/toggle-theme";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1 className="text-4xl font-bold">Welcome to Wow Char Info!</h1>
-      <div className="flex gap-1 font-bold">
-        <span>Char</span>
-        <Image
-          src="/wow-icon.svg"
-          alt="World of Warcraft logo"
-          width={24}
-          height={24}
-        />
-        <span>Info</span>
+      <div>
+        <div className="flex gap-1 font-bold">
+          <span>Char</span>
+          <Image
+            src="/wow-icon.svg"
+            alt="World of Warcraft logo"
+            width={24}
+            height={24}
+          />
+          <span>Info</span>
+        </div>
+        <ModeToggle />
       </div>
     </main>
   );
